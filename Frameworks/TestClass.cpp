c9578e7fd363f55165f527fd2f267986b6a18ad0
kyle <kyleeng003@gmail.com>
2023-06-06 14:06:28 -0400

    Made a test class that is a place holder and can be safely deleted

---
+++ Frameworks/TestClass.cpp
@@ -0,0 +1,13 @@
+//
+// Created by kyle on 6/6/23.
+//
+
+/*
+ *
+ *
+ *   This is a test class and can be deleted
+ *
+ *
+ */
+
+#include "TestClass.h"
---
+++ Frameworks/TestClass.h
@@ -0,0 +1,22 @@
+//
+// Created by kyle on 6/6/23.
+//
+
+/*
+ *
+ *
+ *      This is a test class and can be deleted when done
+ *
+ *
+ */
+
+#ifndef RENNSMOTORSPORT_TESTCLASS_H
+#define RENNSMOTORSPORT_TESTCLASS_H
+
+
+class TestClass {
+
+};
+
+
+#endif //RENNSMOTORSPORT_TESTCLASS_H
