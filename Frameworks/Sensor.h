b1565492eb7f7abba72afd2efc796b8960df40e9
kyle <kyleeng003@gmail.com>
2023-06-09 14:18:07 -0400

    Added simple framework

--- Frameworks/Sensor.h
+++ Frameworks/Sensor.h
@@ -4,9 +4,29 @@
 
 #ifndef RENNSMOTORSPORT_SENSOR_H
 #define RENNSMOTORSPORT_SENSOR_H
-
+#include <string>
 
 class Sensor {
+public:
+    /*
+    bool operator==(Sensor s);
+    bool operator>(Sensor s);
+    bool operator<(Sensor s);
+
+    //clears stored data
+    void clear();
+    */
+private:
+    //member variables:
+
+    //Contains name of the input sensor
+    std::string sensorName;
+
+    //Sample rate of the sensor in Hz
+    int sampleRate;
+
+
+
 
 };
 
