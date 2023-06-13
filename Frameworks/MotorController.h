060c934bdc4f02f82a52d21c55b46d182bc82592
KyleHarsh <duhboss1010900@gmail.com>
2023-06-13 14:19:48 -0400

    added inital basic Functionality for MotorController and Temperature

---
+++ Frameworks/MotorController.h
@@ -0,0 +1,16 @@
+//
+// Created by harshk on 6/13/2023.
+//
+
+#ifndef CONTROLSYSTEMSUNIT_MOTORCONTROLLER_H
+#define CONTROLSYSTEMSUNIT_MOTORCONTROLLER_H
+
+
+
+class MotorController {
+
+};
+
+
+
+#endif //CONTROLSYSTEMSUNIT_MOTORCONTROLLER_H
---
+++ Frameworks/Temperature.h
@@ -0,0 +1,16 @@
+//
+// Created by harshk on 6/13/2023.
+//
+
+#ifndef CONTROLSYSTEMSUNIT_TEMPERATURE_H
+#define CONTROLSYSTEMSUNIT_TEMPERATURE_H
+
+
+
+class Temperature {
+
+};
+
+
+
+#endif //CONTROLSYSTEMSUNIT_TEMPERATURE_H
