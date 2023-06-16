353d9230fc8c8f0683862d0c1af1409c810d23ed
kyle <kyleeng003@gmail.com>
2023-06-15 20:52:13 -0400

    Added a more robust documentation to all current classes

--- Frameworks/MotorController.h
+++ Frameworks/MotorController.h
@@ -1,6 +1,25 @@
-//
-// Created by harshk on 6/13/2023.
-//
+/*
+ * -----------------------------------------------------------------
+ *
+ *   __  __       _                _____            _             _ _
+ *  |  \/  |     | |              / ____|          | |           | | |
+ *  | \  / | ___ | |_ ___  _ __  | |     ___  _ __ | |_ _ __ ___ | | | ___ _ __
+ *  | |\/| |/ _ \| __/ _ \| '__| | |    / _ \| '_ \| __| '__/ _ \| | |/ _ \ '__|
+ *  | |  | | (_) | || (_) | |    | |___| (_) | | | | |_| | | (_) | | |  __/ |
+ *  |_|  |_|\___/ \__\___/|_|     \_____\___/|_| |_|\__|_|  \___/|_|_|\___|_|
+ *
+ * ------------------------------------------------------------------
+ *
+ * Class Function: This class acts as a controller for all motor functions, including all sensors that will be used
+ *                     This class will dictate how other classes interact with each other
+ *
+ * Member Variables:
+ *
+ *
+ * Member functions:
+ *
+ *
+ */
 
 #ifndef CONTROLSYSTEMSUNIT_MOTORCONTROLLER_H
 #define CONTROLSYSTEMSUNIT_MOTORCONTROLLER_H
