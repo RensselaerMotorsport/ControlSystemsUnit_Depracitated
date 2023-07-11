492eaa601b17049a17d1cd2d6e66dec7b4b74024
KEGit28 <Kyleeng003@gmail.com>
2023-07-11 14:39:37 -0400

    Made framework for more specific sensor classes

--- Frameworks/Sensors/BrakePressure.h
+++ Frameworks/Sensors/BrakePressure.h
@@ -1,12 +1,31 @@
-//
-// Created by kyle on 7/11/23.
-//
+/*
+ *
+ *   ____            _          _____
+ *  |  _ \          | |        |  __ \
+ *  | |_) |_ __ __ _| | _____  | |__) | __ ___  ___ ___ _   _ _ __ ___
+ *  |  _ <| '__/ _` | |/ / _ \ |  ___/ '__/ _ \/ __/ __| | | | '__/ _ \
+ *  | |_) | | | (_| |   <  __/ | |   | | |  __/\__ \__ \ |_| | | |  __/
+ *  |____/|_|  \__,_|_|\_\___| |_|   |_|  \___||___/___/\__,_|_|  \___|
+ *
+ *
+ */
 
 #ifndef RENNSMOTORSPORT_BRAKEPRESSURE_H
 #define RENNSMOTORSPORT_BRAKEPRESSURE_H
 
+#include "../AnalogSensor.h"
 
-class BrakePressure {
+class BrakePressure : AnalogSensor<float>{
+public:
+
+    BrakePressure() : AnalogSensor<float>() { raw_signal =-1; pressure =-1; };
+    void transfer_function();
+    float get_pressure();
+
+private:
+
+    float raw_signal;
+    float pressure;
 
 };
 
