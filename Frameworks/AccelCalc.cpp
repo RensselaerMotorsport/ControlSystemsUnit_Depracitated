ba12a28855ec96c355ff2b10e27738e97dc3b90f
KyleHarsh <duhboss1010900@gmail.com>
2023-07-25 18:06:14 -0400

    Added ShockPot class, but not finished

---
+++ Frameworks/AccelCalc.cpp
@@ -0,0 +1,19 @@
+//
+// Created by harshk on 7/25/2023.
+//
+#include <pair>
+#include <vector>
+
+//It is assumed that functions are stored in the form <a0, a1, a2, a3> = a0 + a1*x + a2*x^2 + a3*x^3
+
+std::vector<float> interpoalte(std::vector<std::pair<float, float>> points){
+
+}
+
+std::vector<float> derive(std::vector<float> function){
+
+}
+
+float evaluate(std::vector<float> function){
+
+}
