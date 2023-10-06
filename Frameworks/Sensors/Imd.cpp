283a885d772879639198a55a12e3a19048c1672b
KyleHarsh <duhboss1010900@gmail.com>
2023-10-06 16:40:48 -0400

    Mostly FInished Imd sensor

--- Frameworks/Sensors/Imd.cpp
+++ Frameworks/Sensors/Imd.cpp
@@ -3,3 +3,14 @@
 //
 
 #include "Imd.h"
+
+void Imd::update(std::vector<bool> var) {
+    highResTime callTime = std::chrono::system_clock::now();
+    this->imd_status = var[0];
+    this->running_flag = var[1];
+    this->dataLog.addValue(callTime, this->rotation_angle);
+    std::vector<float> vec = std::vector<bool>(2);
+    vec[0] = this->imd_status;
+    vec[1] = this->running_flag;
+    this->dataLog.addValue(callTime, vec);
+}
--- Frameworks/Sensors/Imd.h
+++ Frameworks/Sensors/Imd.h
@@ -6,23 +6,39 @@
 #define CONTROLSYSTEMSUNIT_IMD_H
 
 #include "./CANSensor.h"
+#include <vector>
 
-class Imd : CANSensor<bool>{
+class Imd : CANSensor<std::vecotr<bool>>{
 public:
     Imd(int frequency, int id)
-            : CANSensor<bool>("Imd", DataLogger<bool>(), frequency, id)
+            : CANSensor<std::vecotr<bool>>("Imd", DataLogger<std::vecotr<bool>>(), frequency, id)
     { imd_status = false; running_flag = false; }
 
-    //TODO: ask Ammar about implementation
-    int get_status();
-    int get_running_flag();
-    int reset_running_flag();
+    int get_status() { return imd_status; }
+    int get_running_flag() { return running_flag; }
+    int reset_running_flag() { running_flag = true; } //TODO: ask ammar if reset means true or false
+
+    //TODO: figure out what input type is
+    void update(std::vector<bool> var) override;
 
 private:
     bool imd_status;
     bool running_flag;
+    // Stored in datalogger as [imd_status, running_flag]
 
 };
 
+// Overloading << operator for std::vector<bool>
+inline std::ostream& operator<<(std::ostream& os, const std::vector<bool>& vec) {
+    for (size_t i = 0; i < vec.size(); ++i) {
+        if (vec[i]){
+            os << 1;
+        } else {
+            os << 0;
+        }
+        if (i != vec.size() - 1) os << ", ";
+    }
+    return os;
+}
 
 #endif //CONTROLSYSTEMSUNIT_IMD_H
