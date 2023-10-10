6069885de71cddc6f754cf1f4dff13f3118336a4
Tre'Vaughn Barboza <trerese3@gmail.com>
2023-10-10 16:19:01 -0400

    Added Documentation

--- Frameworks/Scheduler.h
+++ Frameworks/Scheduler.h
@@ -1,3 +1,34 @@
+/*
+ * -----------------------------------------------------------------
+ *   _____      _              _       _
+ *  / ____|    | |            | |     | |
+ * | (___   ___| |__   ___  __| |_   _| | ___ _ __
+ *  \___ \ / __| '_ \ / _ \/ _` | | | | |/ _ \ '__|
+ *  ____) | (__| | | |  __/ (_| | |_| | |  __/ |
+ * |_____/ \___|_| |_|\___|\__,_|\__,_|_|\___|_|
+ *
+ * ------------------------------------------------------------------
+ *
+ * Class Function: The Scheduler class is responsible for managing and
+ *                 executing tasks in a timely manner. It utilizes a
+ *                 priority queue to manage tasks and ensures they are
+ *                 executed at the appropriate times, facilitating the
+ *                 coordination and management of sensor data and tasks
+ *                 in the control system.
+ *
+ * Member Variables:
+ * - tasks: A priority queue that manages tasks based on their next
+ *          execution time.
+ * - running: A boolean flag indicating whether the scheduler is running.
+ *
+ * Member functions:
+ * - registerSensor: Registers a sensor with the scheduler, creating a
+ *                   task for it in the priority queue.
+ * - run: Begins the scheduler's execution, managing and executing tasks.
+ * - stop: Stops the scheduler from running.
+ * - getCurrentTime: Retrieves the current high-resolution time.
+ *
+ */
 #ifndef RENNSMOTORSPORT_SCHEDULER_H
 #define RENNSMOTORSPORT_SCHEDULER_H
 
--- Frameworks/Task.h
+++ Frameworks/Task.h
@@ -1,3 +1,38 @@
+/*
+ * -----------------------------------------------------------------
+ *  _______        _
+ * |__   __|      | |
+ *    | | __ _ ___| | __
+ *    | |/ _` / __| |/ /
+ *    | | (_| \__ \   <
+ *    |_|\__,_|___/_|\_\
+ *
+ * ------------------------------------------------------------------
+ *
+ * Class Function: The Task class, and its base class TaskBase, manage the
+ *                 execution of tasks related to sensor data retrieval and
+ *                 logging in the control system. Task utilizes a template
+ *                 to accommodate different sensor data types and ensures
+ *                 tasks are executed with consideration to timing and
+ *                 sensor type.
+ *
+ * TaskBase - an abstract base class for Task.
+ *
+ * Member Variables (Task):
+ * - id: An identifier for the task.
+ * - hZ: The frequency (in Hz) at which the task should be executed.
+ * - sensor: A shared pointer to the sensor associated with the task.
+ * - nextExecTime: The next scheduled execution time for the task.
+ *
+ * Member functions (Task):
+ * - execute: Executes the task, retrieving and logging sensor data.
+ * - getNextExecTime: Retrieves the next scheduled execution time.
+ * - setNextExecTime: Sets the next scheduled execution time.
+ * - getHZ: Retrieves the execution frequency of the task.
+ * - getId: Retrieves the identifier of the task.
+ * - operator<: Compares this task with another based on next execution time.
+ *
+ */
 #ifndef RENNSMOTORSPORT_TASK_H
 #define RENNSMOTORSPORT_TASK_H
 
