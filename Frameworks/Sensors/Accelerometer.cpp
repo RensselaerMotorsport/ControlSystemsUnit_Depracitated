079edf4f1d84c5a50fc6c10cd903e78fb2188aec
Jeremy Goldberger <87028711+jgoldberger26@users.noreply.github.com>
2023-10-25 18:35:40 -0400

    Add Accelerometer/Brake Pressure Transfer Funcs

--- Frameworks/Sensors/Accelerometer.cpp
+++ Frameworks/Sensors/Accelerometer.cpp
@@ -6,16 +6,18 @@
 #include <fstream>
 #include "Accelerometer.h"
 
-//Implementation TBD:
-    float Accelerometer::transfer_function_x(float rawVal){
-        return rawVal;
-    }
-    float Accelerometer::transfer_function_y(float rawVal){
-        return rawVal;
-    }
-    float Accelerometer::transfer_function_z(float rawVal){
-        return rawVal;
-    }
+const float kOffsetVolts = 2.5; // 2.5 V at 0 g
+const float kVoltsPerG = 0.440; // 440 mV/g
+
+float Accelerometer::transfer_function_x(float rawVal){
+    return (rawVal - kOffsetVolts) / kVoltsPerG;
+}
+float Accelerometer::transfer_function_y(float rawVal){
+    return (rawVal - kOffsetVolts) / kVoltsPerG;
+}
+float Accelerometer::transfer_function_z(float rawVal){
+    return (rawVal - kOffsetVolts) / kVoltsPerG;
+}
 
 void Accelerometer::update(std::vector<float> var) {
     highResTime callTime = std::chrono::system_clock::now();
